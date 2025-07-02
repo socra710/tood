19dce4ca4fa4e342cdb8c640f849ee94ac4f4057
김기형 <socra710@gmail.com>
2025-07-02 17:48:12 +0900

    타이틀 수정

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,14 @@
 <header>
-  <h1>Wat밥</h1>
+  <h1>
+    Wat밥
+    <img
+      class="logo-img"
+      src="/favicon.png"
+      alt="Wat밥 로고"
+      width="32"
+      height="32"
+    />
+  </h1>
 </header>
 
 <style>
@@ -9,5 +18,16 @@
     padding: 0.1rem;
     text-align: center;
     letter-spacing: 1px;
+  }
+  .logo-img {
+    background: #fff;
+    border-radius: 50%;
+    padding: 0px;
+    box-sizing: content-box;
+    box-shadow: 0 0 0 2px #fff;
+    vertical-align: middle;
+    /* 로고를 위로 약간 이동 */
+    position: relative;
+    top: -5px; /* 숫자가 클수록 더 위로 */
   }
 </style>
